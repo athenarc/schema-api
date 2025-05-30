1352c070e4df01f917d7c2be5afb59ff1919ac0b
tzerefos <tzerefos@athenarc.gr>
2025-05-30 15:12:24 +0300

    Add the url for the file preview

--- schema-api/files/urls.py
+++ schema-api/files/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from files.views import FilesListAPIView, FileDetailsAPIView
+from files.views import FilesListAPIView, FileDetailsAPIView, FilePreviewAPIView
 
 urlpatterns = [
     path('files', FilesListAPIView.as_view(), name='files_list'),
-    path('files/<path:path>', FileDetailsAPIView.as_view(), name='file_details')
+    path('files/preview', FilePreviewAPIView.as_view(), name='file-preview'),
+    path('files/<path:path>', FileDetailsAPIView.as_view(), name='file_details'),
 ]
